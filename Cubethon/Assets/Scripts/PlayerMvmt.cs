0072455654abac1b33dd82c7f29717cbb4449ab7
359Kirk <brianabishopaz@gmail.com>
2022-03-27 21:51:08 -0700

    Cubethon Production added

--- Cubethon/Assets/Scripts/PlayerMvmt.cs
+++ Cubethon/Assets/Scripts/PlayerMvmt.cs
@@ -7,6 +7,7 @@ public class PlayerMvmt : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    public float upwardForce = 50f;
     // Update is called once per frame
     void FixedUpdate ()
     {
@@ -22,7 +23,33 @@ public class PlayerMvmt : MonoBehaviour
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-        if(rb.position.y < -1f)
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, -upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -25f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        if (rb.position.y > 25f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        if (rb.position.x < -25f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        if (rb.position.x > 25f)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
