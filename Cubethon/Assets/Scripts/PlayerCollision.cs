57af387eb21dcff9bd0246c5305ac6184e987e69
359Kirk <brianabishopaz@gmail.com>
2022-03-28 10:34:57 -0700

    Production Assignment done

--- Cubethon/Assets/Scripts/PlayerCollision.cs
+++ Cubethon/Assets/Scripts/PlayerCollision.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMvmt movement;
+    public Collider boxCollider;
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
@@ -12,5 +13,24 @@ public class PlayerCollision : MonoBehaviour
             movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
         }
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            boxCollider.enabled = !boxCollider.enabled;
+        }*/
+
+        
+        /*(gameObject.GetComponent(typeof(BoxCollider)) as BoxCollider).enabled = true;
+        {
+            if (Input.GetKey("g"))
+            {
+                (gameObject.GetComponent(typeof(BoxCollider)) as BoxCollider).enabled = false;
+            }
+
+            if (Input.GetKey("n"))
+            {
+                (gameObject.GetComponent(typeof(BoxCollider)) as BoxCollider).enabled = true;
+            }
+        }*/
     }
 }
